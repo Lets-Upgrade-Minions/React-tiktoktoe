61076c73d69813e7f5f3b8cf54f34c4a662370ec
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2022-01-07 19:30:17 +0530

    tik tok toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Icon from "./Icon/Icon"
+// importing Toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  
+  let tiktokArray = new Array(9).fill("")
+  let [winMessage, setWinMessage] = useState("")
+  let [isCross, setIsCross] = useState(true)
+  
+  // game reload logic 
+  const reloadGame = () => {
+    tiktokArray.fill("")
+    setWinMessage("")
+    setIsCross(true)
+  }
+
+  // win Logic 
+  const checkWinner = () => {
+      // winner and draw logic
+  }
+
+  // mark your array 
+  const markTiktokArray = (index) => {
+        if(winMessage){
+            return toast.error('The Game is already over', {
+              position: "bottom-center",
+              });
+        }
+        else if(tiktokArray[index] !== ""){
+            return toast.error('Already Marked', {
+              position: "bottom-center",
+              });
+        }
+        else{
+          tiktokArray[index] =  isCross ? "cross" : "circle"
+          setIsCross(!isCross)
+        }
+        checkWinner()
+  }
+
+
+// ui
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Icon />
     </div>
   );
 }
-
 export default App;
